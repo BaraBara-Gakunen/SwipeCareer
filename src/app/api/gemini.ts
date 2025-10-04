30dff9c030da3f25ceff0bd2c15c1102a5ab7aa6
Haruto Taketomi <takebamboo88@gmail.com>
2025-10-04 17:00:57 +0900

    feature: Gemini-APIの実装
    method type: POST
    引数: MatchedCompanyをbodyに取ったJSON
    
    .envファイルはDiscordに配布済みである
    APIフォルダの位置は統合後変更の可能性大

---
+++ src/app/api/gemini.ts
@@ -0,0 +1,20 @@
+import { GoogleGenAI } from "@google/genai";
+import { makePrompt, Instruction } from "./prompt";
+import { MatchedCompany } from "@/types/interface";
+
+
+export const makeESDraft = async (content: MatchedCompany): Promise<string> => {
+  const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_GEMINI_API_KEY, });
+
+  const response = await ai.models.generateContent
+  ({
+    model: "gemini-2.5-flash",
+    contents: makePrompt(content),
+    config: {
+      thinkingConfig: { includeThoughts: false },
+      temperature: 0.6,
+      systemInstruction: Instruction,
+    }
+  });
+  return response.text ? response.text : "";
+};
--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,19 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
-import { GoogleGenAI } from "@google/genai";
+import { makeESDraft } from "./gemini";
+// import { mockMatchedCompany } from "./mockObject";
 
-export async function POST(req: NextRequest) {
-  const content = await req.json();
-  const ai = new GoogleGenAI({
-    apiKey: process.env.GOOGLE_GEMINI_API_KEY,
-  });
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  try {
+    const inputData = await req.json().then((data) => data.content);
+    // const inputData = mockMatchedCompany;
 
-  const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: content.content,
-    config: {
-      temperature: 0.7,
-    }
-  });
+    const es = await makeESDraft(inputData);
+    console.debug("Generated ES: \r\n====================", es, "\r\n====================\r\n");
+    return NextResponse.json({ success: true, es });
 
-  return NextResponse.json({ text: response.text });
+  } catch (error) {
+    console.debug("Error in API route:", error);
+    return NextResponse.json(
+      { 
+        success: false, 
+        error: error instanceof Error ? error.message : "Unknown error" 
+      },
+      { status: 500 }
+    );
+  }
 }
